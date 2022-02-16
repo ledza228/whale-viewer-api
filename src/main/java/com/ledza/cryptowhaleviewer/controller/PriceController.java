68a5526f6330f0e5d3b117ea1e3600827de942bc
ledza228 <leshyash2003@gmail.com>
2022-02-16 21:40:26 +0300

    added cross-origin

--- src/main/java/com/ledza/cryptowhaleviewer/controller/PriceController.java
+++ src/main/java/com/ledza/cryptowhaleviewer/controller/PriceController.java
@@ -4,12 +4,14 @@ import com.ledza.cryptowhaleviewer.service.PriceService;
 import com.ledza.cryptowhaleviewer.utils.RequestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin("*")
 public class PriceController {
 
     @Autowired
