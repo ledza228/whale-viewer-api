b767f1037cc74cd07c27ae0b7af3e395c753d314
ledza228 <leshyash2003@gmail.com>
2022-02-19 22:27:05 +0300

    cache updating feature

--- src/main/java/com/ledza/cryptowhaleviewer/CryptoWhaleViewerApplication.java
+++ src/main/java/com/ledza/cryptowhaleviewer/CryptoWhaleViewerApplication.java
@@ -3,14 +3,14 @@ package com.ledza.cryptowhaleviewer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCaching
 @SpringBootApplication
+@EnableScheduling
 public class CryptoWhaleViewerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ledza/cryptowhaleviewer/controller/PriceController.java
+++ src/main/java/com/ledza/cryptowhaleviewer/controller/PriceController.java
@@ -1,15 +1,17 @@
 package com.ledza.cryptowhaleviewer.controller;
 
 import com.ledza.cryptowhaleviewer.service.PriceService;
-import com.ledza.cryptowhaleviewer.utils.RequestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 @RequestMapping("/api/v1/")
 @CrossOrigin("*")
 public class PriceController {
@@ -21,6 +23,11 @@ public class PriceController {
     @Cacheable("price")
     public String getBitcoinHistoryAPI(){
         return  priceService.getBitcoinHistoryPrice();
+    }
+
+    @CacheEvict(cacheNames = {"price"},allEntries = true)
+    @Scheduled(fixedDelay = 60*60*2*1000)
+    public void cacheEvict() {
     }
 
 }
